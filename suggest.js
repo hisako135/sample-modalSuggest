4b28e9ef69ecc79a9af2b5f08966d2b5cbd361fc
白崎 寿子 <baiqi_shouzi@hotmail.com>
2017-09-08 02:32:21 +0900

    ざっくりできた

--- suggest.js
+++ suggest.js
@@ -1,52 +1,61 @@
 $(function(){
-    var fwBody = $('#js-fwBody');
-    var fwBox = $('#js-fwBox');
-    var modal = $('#js-modal');
-    var sgst = $('#js-sgst');
-    var srchBtn = $('#js-srch');
-    var closeBtn = $('#js-close');
-    var sgBody = $('js-sgBody');
-    var sgList = $('js-sgList');
+    var fwBody      = $('#js-fwBody');
+    var fwBox       = $('#js-fwBox');
+    var modal       = $('#js-modal');
+    var kwrd        = $('#js-kwrd');
+    var srchBtn     = $('#js-srch');
+    var closeBtn    = $('#js-close');
+    var sgBody      = $('#js-sgBody');
+    var sgList      = $('#js-sgList');
+    var jsonData    = [];
+    var liTemp      = [];
 
     fwBox.focus(function(){
         fwBody.hide();
         modal.show();
-        
-        closeBtn.on('click',function(){
-            console.log('入力された値は「' + sgst.val() + '」だよ');
-            sgst.val('');
-            console.log(sgst.val() + '消したよ');
-            fwBody.show();
-            modal.hide();
-        });
+        kwrd.focus();
+    });
 
-        srchBtn.on('click',function(){
-            var fw = sgst.val();
-            $.ajax({
-                url: "http://completion.amazon.co.jp/search/complete",
-                data: {mkt:'6', method:'completion', 'search-alias':'aps', q:fw},
-                dataType: "jsonp",
-                type: "GET",
-                success :function(data) {                    
-                    if(data){
-                        var jsonData = data[1];
-                        var liTemp = [];
-                        console.log(jsonData);
-
-                        for(var i = 0; i < jsonData.length; i++){
-                            var li = document.createElement('li');
-                            li.innerHTML = jsonData[i];
-                            liTemp.push(li);
-                        }
-                        console.log(liTemp);
-                        render(sgList,liTemp);
-                        sgBody.css('visibilty','visible');
+    srchBtn.on('click',function(){
+        var fw = kwrd.val();
+        $.ajax({
+            url: "http://completion.amazon.co.jp/search/complete",
+            data: {mkt:'6', method:'completion', 'search-alias':'aps', q:fw},
+            dataType: "jsonp",
+            type: "GET",
+            success :function(data) {
+                jsonData = data[1];                    
+                if(!jsonData.length){
+                    console.log('商品はありません。');
+                }else{
+                    for(var i = 0; i < jsonData.length; i++){
+                        var li ='<li class="list-group-item"><a href="#">'+ jsonData[i] + '</a></li>';
+                        liTemp.push(li);
                     }
+                    render(sgList,liTemp);
+                    sgBody.css('visibility','visible');
                 }
-            });
+            }
         });
     });
+
+    closeBtn.on('click',function(){
+        if(jsonData.length){
+            reset(jsonData);
+            reset(liTemp);
+            sgList.empty();
+        }
+        kwrd.val('');
+        modal.hide();
+        fwBody.show();
+    });  
+
     function render(dom,html){
-        dom.innerHTML = html;
+        return dom.append(html);
     }
+
+    function reset(array){
+        return array.splice(0,array.length);
+    }
+
 });
